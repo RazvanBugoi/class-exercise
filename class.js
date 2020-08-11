3ba1c85be0fb8b241dc34d8e27d71908eed4937e
RazvanBugoi <46108577+RazvanBugoi@users.noreply.github.com>
2020-08-11 21:30:04 +0100

    created classes and used extends and super keywords

---
+++ class.js
@@ -0,0 +1,35 @@
+class Person {
+  constructor(firstName, lastName, age, gender, interests) {
+    this.name = `${firstName} ${lastName}`;
+    this.age = age;
+    this.gender = gender;
+    this.interests = interests;
+  }
+  bio() {
+    return `${this.name} is ${this.age} years old. They like ${this.interests }.`;
+  }
+  greeting() {
+    return `Hi! I'm ${this.name}`;
+  }
+}
+
+
+class Teacher extends Person {
+  constructor(firstName, lastName, age, gender, interests, subject) {
+    super(firstName, lastName, age, gender, interests);
+    this.subject = subject;
+  }
+  greeting() {
+    return `Hello. My name is ${this.name}, I am ${this.age} years old and I teach ${this.subject}.`;
+  }
+};
+
+
+class Student extends Person {
+  constructor(firstName, lastName, age, gender, interests) {
+    super(firstName, lastName, age, gender, interests);
+  }
+  greeting() {
+    return `Yo! I'm ${this.name.split(' ')[0]}`;
+  }
+}
